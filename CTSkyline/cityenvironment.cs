5c6f3c9460114672b56d3b4f5cb0293f6be5a380
OrangeZone <hyunjae000467@gmail.com>
2023-08-16 20:04:12 +0900

    초기 커밋.

---
+++ CTSkyline/cityenvironment.cs
@@ -0,0 +1,12 @@
+﻿using ICities;
+using UnityEngine;
+
+namespace CTenv
+{
+
+    public class EnvMod : IUserMod
+    {
+        public string Name => "City Environment Research";
+        public string Description => "City Research by 황현재 김수민 채우린, Developed by CE-L Division 1";
+    }
+}
---
+++ CTSkyline/cityenvironmentloading.cs
@@ -0,0 +1,33 @@
+﻿using ColossalFramework.UI;
+using ICities;
+
+namespace CTenv
+{
+    public class CTenv : ILoadingExtension
+    {
+        // called when level loading begins
+        public void OnCreated(ILoading loading)
+        {
+        }
+
+        // called when level is loaded
+        public void OnLevelLoaded(LoadMode mode)
+        {
+            // create dialog panel
+            ExceptionPanel panel = UIView.library.ShowModal<ExceptionPanel>("ExceptionPanel");
+
+            // display a message for the user in the panel
+            panel.SetMessage("CTenv", "연구 모드가 정상적으로 로딩되었습니다.", false);
+        }
+
+        // called when unloading begins
+        public void OnLevelUnloading()
+        {
+        }
+
+        // called when unloading finished
+        public void OnReleased()
+        {
+        }
+    }
+}
